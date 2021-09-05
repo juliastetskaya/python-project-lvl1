199c175c19e21e702ac35bef6e53a1ac8b5c36db
Julia Stetskaya <juliastetskaya@gmail.com>
2021-09-05 12:10:44 +0300

    Add a prompt for username

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-
-def welcome():
-    print('Welcome to the Brain Games!')
+from brain_games.cli import welcome_user
 
 def main():
-    welcome()
+    welcome_user()
 
 if __name__ == "__main__":
     main()
